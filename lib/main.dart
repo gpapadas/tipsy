9ed26d16101c43e056fa92f072ec99ed346059ec
gpapadas@gmail.com <gpapadas@gmail.com>
2020-05-03 21:30:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:tipsy/home_screen.dart';
+void main() => runApp(TipsyApp());
+
+class TipsyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
